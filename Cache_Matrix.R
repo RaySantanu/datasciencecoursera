5e06b47cd9cfdb410ed22d78131d922c0b106c8d
RaySantanu <Ray.santanu@icloud.com>
2017-09-08 16:56:23 +0530

    SantanuRay_RCode_Matrix Inversion _Cache Memory
    Course Era 
    Peer-graded Assignment: Programming Assignment 2: Lexical Scoping

---
+++ Cache_Matrix.R
@@ -0,0 +1,28 @@
+#Created by Santanu Ray.Email id :ray.santanu@icloud.com
+mak_inv <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+  
+#x<-mak_inv(matrix(1:4,nrow=2,ncol=2))
+cache_inv <- function(x, ...) {
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+}
+#cache_inv(x)
